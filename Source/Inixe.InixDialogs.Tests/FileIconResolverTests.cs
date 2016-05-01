07e183e4d4d5f7b258f7913171ae9c62e09922d4
ana <anabelvb@inixe.com.mx>
2016-04-30 22:55:13 -0500

    Initial commit to add default .gitIgnore and .gitAttribute files.

---
+++ Source/Inixe.InixDialogs.Tests/FileIconResolverTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Inixe.InixDialogs.Tests
+{
+	[TestClass]
+	public class FileIconResolverTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
